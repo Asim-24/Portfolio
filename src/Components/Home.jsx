e97a8913bbb19cd5a81bc5857e42c481febd1848
Asimshk24 <shaikhasim.mail@gmail.com>
2026-08-25 23:27:51 +0530

    resume added

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -21,7 +21,7 @@ const Home = () => {
 
             <a href="https://github.com/Asim-24" target='_blank' rel='noopener noreferrer'><FontAwesomeIcon icon={faGithub} className='text-black p-1 ml-4 bg-green-900 rounded-xl text-3xl origin-bottom hover:scale-x-110 hover:scale-y-110 hover:text-black hover:bg-white transition-all duration-300 ease-in-out ' /></a>
             <a href="https://linkedin.com/in/asimshaikh24" target='_blank' rel='noopener noreferrer'> <FontAwesomeIcon icon={faLinkedin} className='text-black bg-green-900 rounded-xl p-1 text-3xl origin-bottom hover:scale-x-110 hover:scale-y-110 hover:text-black hover:bg-white transition-all duration-300 ease-in-out' /></a>
-            <a href="#" className='text-white p-1 text-xl text-center border-2 ml-5 border-white h-auto w-32 rounded-xl pb-2 active:bg-green-900 hover:bg-white hover:text-black transition-all duration-500 ease-in-out '>Resume</a>
+            <a href="https://drive.google.com/file/d/1sHt_ThKk8JRRuJ_BMCwgTWwtc_EUBfps/view?usp=sharing" className='text-white p-1 text-xl text-center border-2 ml-5 border-white h-auto w-32 rounded-xl pb-2 active:bg-green-900 hover:bg-white hover:text-black transition-all duration-500 ease-in-out '>Resume</a>
             
            </div>
 
